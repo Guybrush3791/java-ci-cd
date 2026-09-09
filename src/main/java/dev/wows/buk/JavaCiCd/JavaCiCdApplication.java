cdbbca4af251a199cb79f60373d0bee80164d429
Guybrush <guybrush@wows.dev>
2026-09-09 21:29:06 +0200

    init

---
+++ src/main/java/dev/wows/buk/JavaCiCd/JavaCiCdApplication.java
@@ -0,0 +1,13 @@
+package dev.wows.buk.JavaCiCd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wows/buk/JavaCiCd/JavaCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wows.buk.JavaCiCd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
